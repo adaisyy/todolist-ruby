d75b34db0cc445b4f79fd6b98f675801d0cc2028
adaisyy <04ksrai02@gmail.com>
2018-10-16 12:00:40 +0900

    fix : routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # / ルートアクセスでTODOリストが表示されるように変更
-  root :to => 'todolists#index'
+  root 'todolists#index'
 end
