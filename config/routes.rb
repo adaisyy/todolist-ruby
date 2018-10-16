08997398bcc34250cc2b1896cccce2e53e4e34c8
adaisyy <04ksrai02@gmail.com>
2018-10-16 11:54:24 +0900

    fix : routes, del : index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # TODOリストルーティング
-  resources :todolists
+  # / ルートアクセスでTODOリストが表示されるように変更
+  root :to => 'todolists#index'
 end
