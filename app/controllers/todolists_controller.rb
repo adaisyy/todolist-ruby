577d8848025864fe79821677fc4e65f378ebca00
adaisyy <04ksrai02@gmail.com>
2018-10-31 14:48:28 +0900

    fix : todolist

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,12 +1,21 @@
-=begin
-    TODOリストコントローラ
-    @author adaisyy
-    @date   2018/10/11
-=end
-
 class TodolistsController < ApplicationController
     def index
         # todoリストの取得
         @todolists = Todolist.all
     end
+
+    def new
+        @todolist = Todolist.new
+    end
+
+    def create
+        @todolist = Todolist.new(input_params)
+        @todolist.save
+        redirect_to root_path
+    end
+
+    private
+        def input_params
+            params.require(:todolist).permit(:deadline, :task)
+        end
 end
