d70657f6c85c6ad5687a2135daf9a1be0200228e
adaisyy <04ksrai02@gmail.com>
2018-10-31 14:46:40 +0900

    fix : column, add : timezone

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_075100) do
+ActiveRecord::Schema.define(version: 2018_10_23_025611) do
 
   create_table "todolists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.date "date"
+    t.datetime "deadline"
     t.text "task"
-    t.integer "fin_flag", limit: 1, default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
